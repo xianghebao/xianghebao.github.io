7a98f70c04e9bead1c57cfc9f5797b2fcc657618
xhb <992201289@qq.com>
2024-05-05 13:41:41 +0800

    feat docs

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -17,5 +17,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/xianghebao.github.io/"
+  base: "/xianghebao.github.io/",
+  build:{
+    outDir: "docs"
+  }
 })
